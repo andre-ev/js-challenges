63260876efc5dfc0aaf76db0bcdb3480c19a7aed
André Escobar <andre.ev@outlook.com>
2023-03-16 19:04:03 -0500

    solved exercises

--- src/factorial.js
+++ src/factorial.js
@@ -1,5 +1,6 @@
 const factorial = (number) => {
   // your code here
+  return number < 0 ? null : number <= 1 ? 1 : number * factorial(number - 1);
 }
 
 module.exports = factorial;
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,20 @@
 const fibonacci = (n) => {
-  // your code here
+  const array = [];
+
+  let a = 0;
+  let b = 1;
+
+  for (let i = 0; i < n; i++) {
+    let c = a + b;
+
+    a = b;
+    b = c;
+
+    array.push(a);
+  }
+
+  return array;
+
 }
 
 module.exports = fibonacci;
